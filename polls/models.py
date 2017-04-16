291ea2c9553d2da01358006b6b70dc4e7c345fdb
Dave Packer <dave78packer@gmail.com>
2017-04-16 16:49:11 -0600

    everything up to tutorial 6

--- polls/models.py
+++ polls/models.py
@@ -7,12 +7,13 @@ from django.utils.encoding import python_2_unicode_compatible
 # Create your models here.
 @python_2_unicode_compatible
 class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
     def __str__(self):
         return self.question_text
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 @python_2_unicode_compatible
 class Choice(models.Model):
